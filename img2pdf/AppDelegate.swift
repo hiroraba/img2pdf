30c1a853af6adde0472cf725630ad6b2575daa84
hiroraba <xornelius0313@gmail.com>
2025-03-26 23:47:17 +0900

    swiftlint

--- img2pdf/AppDelegate.swift
+++ img2pdf/AppDelegate.swift
@@ -11,19 +11,6 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
 
-        let windowSize = NSRect(x: 0, y: 0, width: 400, height: 200)
-        window = NSWindow(contentRect: windowSize, styleMask: [.titled, .closable, .miniaturizable, .resizable], backing: .buffered, defer: false)
-        window.center()
-        window.title = "Img2pdf"
-        
-        let viewController = ViewController()
-        window.contentViewController = viewController
-        window.makeKeyAndOrderFront(nil)
-    }
-
-
+    func applicationDidFinishLaunching(_ aNotification: Notification) {}
 }
-
--- img2pdf/DragDropView.swift
+++ img2pdf/DragDropView.swift
@@ -8,46 +8,43 @@
 import Cocoa
 
 class DragDropView: NSView {
-    
-    var fileURLs: [URL] = []
-    {
+
+    var fileURLs: [URL] = [] {
         didSet {
             print("ファイルが追加されました\(fileURLs)")
         }
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         registerForDraggedTypes([.fileURL])
     }
-    
+
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         registerForDraggedTypes([.fileURL])
         wantsLayer = true
         layer?.backgroundColor = NSColor.lightGray.cgColor
     }
-    
+
     override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
         if containsPNGFile(draggingInfo: sender) {
             return .copy
         }
         return []
     }
-    
+
     override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
         let pasteboard = sender.draggingPasteboard
         if let urls = pasteboard.readObjects(forClasses: [NSURL.self], options: nil) as? [URL] {
-            for url in urls {
-                if url.pathExtension == "png" {
-                    fileURLs.append(url)
-                }
+            for url in urls where url.pathExtension == "png" {
+                fileURLs.append(url)
             }
             return true
         }
         return false
     }
-    
+
     func containsPNGFile(draggingInfo: NSDraggingInfo) -> Bool {
         let pasteboard = draggingInfo.draggingPasteboard
         if let urls = pasteboard.readObjects(forClasses: [NSURL.self], options: nil) as? [URL] {
